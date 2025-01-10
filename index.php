ae88fcf47440260c6b8989be28b9220187d0ad9f
iqbalzeeshan <iqbalzeeshan@hotmail.com>
2025-01-10 16:55:46 +0000

    db test

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
-echo 'i am here';
+$servername = "scaleabledatabase.mysql.database.azure.com";
+$database = "showvideodb";
+$username = "adminzeeshan";
+$password = "Zeeshan@786";
+
+// Create connection
+$conn = new mysqli($servername, $database, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo ' db is okk'
 ?>
 
 <!DOCTYPE html>
