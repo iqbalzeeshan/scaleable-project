dcc272566deded13a05e63acd80e83640fdbf653
iqbalzeeshan <iqbalzeeshan@hotmail.com>
2025-01-12 19:43:16 +0000

    just change the title of index page

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ if ($conn->connect_error) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Reels Page</title>
+    <title>Video Show App</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
         body {
